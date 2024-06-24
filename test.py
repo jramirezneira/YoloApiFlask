b1de7dd1b0871c6018896ebad5d04ba692ff9c06
unknown <jramirezneira@gmail.com>
2024-06-24 22:06:57 +0200

    nv

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
+from flask import Flask, jsonify, request, Response
+
+app = Flask(__name__)
 print("This line will be printed.")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True,  port=5001)
